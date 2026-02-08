0adefce6a8adaaa1521fbafac91654520efe142e
Hossein Garosian <hosseingarosian09@gmail.com>
2026-02-08 04:26:23 +0330

    made a loop to find each element in each page . and some lines to find the titel-brand .

--- divar-scrape.py
+++ divar-scrape.py
@@ -13,3 +13,16 @@ links = []
 for card in cards:
     href = card.get('href')
     links.append('https://divar.ir' + href)
+
+#finding each element in each page 
+for link in links :
+  request = requests.get(link,timeout=5)
+  soup = BeautifulSoup(request.text , "html.parser")
+
+  titel_brand = None
+  try:
+    titel_brand = soup.find(class_="kt-unexpandable-row__action kt-text-truncate").text
+  except TypeError:
+    titel_brand = soup.find(class_="kt-base-row__end kt-unexpandable-row__value-box").text
+  else : 
+    titel_brand = None
