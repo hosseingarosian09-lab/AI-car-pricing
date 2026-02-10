e297eced938ff67efed6ed10ddb7d46b39a3cc8a
hossein <hosseingarosian09@gmail.com>
2026-02-11 01:53:31 +0330

    renamed divar_scrape --> main.py moved  the scrape code to another file . made a list of info

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests 
+from bs4 import BeautifulSoup
+from random_headers import get_random_headers
+from divar_scrape import extract_car_info
+
+#finding all links in the page
+request = requests.get("https://divar.ir/s/iran/auto")
+
+print(request.status_code)
+
+soup = BeautifulSoup(request.text , "html.parser")
+cards = soup.find_all('a', class_='kt-post-card__action')
+
+links = []
+for card in cards:
+    href = card.get('href')
+    links.append('https://divar.ir' + href)
+
+for link in links :
+    # generating random headers
+    headers = get_random_headers(str(link))
+
+    request = requests.get(link,timeout=10, headers=headers)
+    if request.status_code != 200:
+            print(f"skiped for error: \n{request.status_code}")
+            continue
+    
+    info_list = extract_car_info(request)
+
+    print(
+         "titel_brand: ", info_list[0], "\n",
+         "kilometer: ", info_list[1], "\n",
+         "year: ", info_list[2], "\n",
+         "color: ", info_list[3], "\n",
+         "gearbox: ", info_list[4], "\n",
+         "fule: ", info_list[5], "\n",
+         "price: ", info_list[6], "\n",
+         "body_condition: ", info_list[7], "\n",
+         "discription: ", info_list[8], "\n",
+         "pictuer: ", info_list[9], "\n",
+         "-----------------------------"
+    )
